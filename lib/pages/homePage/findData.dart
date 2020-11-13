65a467a2da679ed0e17b0ab0b5df36e4cc22aea3
lmh <207374233@qq.com>
2020-11-13 18:59:25 +0800

    3

---
+++ lib/pages/homePage/findData.dart
@@ -0,0 +1,56 @@
+List findData = [
+  {
+    'username': "张三",
+    'avator':
+        "https://ergouzi.oss-ap-southeast-2.aliyuncs.com/flutter_zuiyou/2.jpg",
+    'sex': 0,
+    'desc': '18岁\n江苏\n聊天的有无\n可爱又气人',
+    'start': 4554,
+    'islike': true
+  },
+  {
+    'username': "韶华的华",
+    'avator':
+        "https://ergouzi.oss-ap-southeast-2.aliyuncs.com/flutter_zuiyou/3.jpg",
+    'sex': 1,
+    'desc': '这个人很懒\n什么都没有留下',
+    'start': 1025,
+    'islike': false
+  },
+  {
+    'username': "海绵宝宝",
+    'avator':
+        "https://ergouzi.oss-ap-southeast-2.aliyuncs.com/flutter_zuiyou/4.jpg",
+    'sex': 1,
+    'desc': '我住在海里\n我的老板是蟹老板\n嘿嘿嘿',
+    'start': 2521,
+    'islike': false
+  },
+  {
+    'username': "张三",
+    'avator':
+        "https://ergouzi.oss-ap-southeast-2.aliyuncs.com/flutter_zuiyou/2.jpg",
+    'sex': 0,
+    'desc': '18岁\n江苏\n聊天的有无\n可爱又气人',
+    'start': 1025,
+    'islike': true
+  },
+  {
+    'username': "韶华的华",
+    'avator':
+        "https://ergouzi.oss-ap-southeast-2.aliyuncs.com/flutter_zuiyou/3.jpg",
+    'sex': 1,
+    'desc': '这个人很懒\n什么都没有留下',
+    'start': 10,
+    'islike': false
+  },
+  {
+    'username': "海绵宝宝",
+    'avator':
+        "https://ergouzi.oss-ap-southeast-2.aliyuncs.com/flutter_zuiyou/4.jpg",
+    'sex': 1,
+    'desc': '我住在海里\n我的老板是蟹老板\n嘿嘿嘿',
+    'start': 666,
+    'islike': true
+  }
+];
---
+++ lib/pages/homePage/messageData.dart
@@ -0,0 +1,28 @@
+List messageData = [
+  {
+    'username': '惊声尖叫',
+    'avator': 'lib/images/2.jpg',
+    'lastMessage': '在吗？接我2000块买演唱会门票'
+  },
+  {
+    'username': '嘿嘿pdf',
+    'avator': 'lib/images/3.jpg',
+    'lastMessage': '在吗？接我2000块买演唱会门票'
+  },
+  {
+    'username': '诉讼费lll',
+    'avator': 'lib/images/4.jpg',
+    'lastMessage': '你在做什么，有没有钱接我'
+  },
+  {
+    'username': 'En冻结',
+    'avator': 'lib/images/2.jpg',
+    'lastMessage': '我是桂生，借我100万'
+  },
+  {
+    'username': '诉讼费lll',
+    'avator': 'lib/images/4.jpg',
+    'lastMessage':
+        '你在做什么，有没有钱接我你在做什么，有没有钱接我你在做什么，有没有钱接我你在做什么，有没有钱接我你在做什么，有没有钱接我你在做什么，有没有钱接我'
+  },
+];
